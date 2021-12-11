187e2f5f1f86ad7d06a137ab229a3d082c690892
Mylene <pont.mylene@gmail.com>
2021-12-11 15:53:32 +0900

    styling of navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,13 @@
 import React from "react"
+import Image from "../images/logo.png"; 
 
 export default function Navbar() {
-    return <h1>Navbar component</h1>
+    return (
+        <nav>
+            <img src={Image} className="nav--icon" alt="react-logo"/>
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
 }
+
