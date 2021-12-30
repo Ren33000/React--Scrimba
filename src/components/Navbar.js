39dc3f0a184c6b49cafd78e4f04e8413e30038fc
Mylene <pont.mylene@gmail.com>
2021-12-30 11:58:23 +0900

    darkmode and light mode switch ok

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,8 @@ export default function Navbar(props) {
         <nav className={props.darkMode ? "dark" : ""}>
             <img src={Image} className="nav--icon" alt="react-logo"/>
             <h3 className="nav--logo_text">ReactFacts</h3>
-            <h4 className="nav--title">React Course - Project 1</h4>
+            {/* <h4 className="nav--title">React Course - Project 1</h4> */}
+
             <div className="toggler">
                 <p className="toggler--light">Light</p>
                 <div className="toggler--slider" onClick={props.toggleDarkMode}>
